5f132322044ff24322b816c096b3b7e5374adb06
wenbang24 <wenbangalt@gmail.com>
2024-10-19 15:35:35 +1100

    welp there's two dice now

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -7,6 +7,7 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -36,23 +37,47 @@ public class App extends Application {
         CameraTransformer cameraTransform = new CameraTransformer();
         cameraTransform.getChildren().add(camera);
 
-        CuboidMesh dice = new CuboidMesh(10f, 10f, 10f);
-        dice.setTextureModeImage("texture.jpg");
+        CuboidMesh dice1 = new CuboidMesh(10f, 10f, 10f);
+        dice1.setTextureModeImage("texture.jpg");
+        CuboidMesh dice2 = new CuboidMesh(10f, 10f, 10f);
+        dice2.setTextureModeImage("texture.jpg");
+        Translate translate = new Translate(20, 0, 0);
+        dice2.getTransforms().add(translate);
 
-        Rotate rX = new Rotate(0, Rotate.X_AXIS);
-        Rotate rY = new Rotate(0, Rotate.Y_AXIS);
-        Rotate rZ = new Rotate(0, Rotate.Z_AXIS);
-        dice.getTransforms().addAll(rX, rY, rZ);
+        Rotate rX1 = new Rotate(0, Rotate.X_AXIS);
+        Rotate rY1 = new Rotate(0, Rotate.Y_AXIS);
+        Rotate rZ1 = new Rotate(0, Rotate.Z_AXIS);
+        dice1.getTransforms().addAll(rX1, rY1, rZ1);
+        Rotate rX2 = new Rotate(0, Rotate.X_AXIS);
+        Rotate rY2 = new Rotate(0, Rotate.Y_AXIS);
+        Rotate rZ2 = new Rotate(0, Rotate.Z_AXIS);
+        dice2.getTransforms().addAll(rX2, rY2, rZ2);
         Random r = new Random();
 
-        AnimationTimer timer = new AnimationTimer() {
+        AnimationTimer timer1 = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                rX.setAngle(rX.getAngle() + 5 + r.nextInt(10));
-                rY.setAngle(rY.getAngle() + 5 + r.nextInt(10));
-                rZ.setAngle(rZ.getAngle() + 5 + r.nextInt(10));
+                rX1.setAngle(rX1.getAngle() + 5 + r.nextInt(10));
+                rY1.setAngle(rY1.getAngle() + 5 + r.nextInt(10));
+                rZ1.setAngle(rZ1.getAngle() + 5 + r.nextInt(10));
 
-                if (isFaceUp(rX)) {
+                if (isFaceUp(rX1)) {
+                    System.out.println("Face aligning " + counter + " more times");
+                    counter -= 1;
+                }
+                if (counter == 0) {
+                    stop();
+                }
+            }
+        };
+        AnimationTimer timer2 = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                rX2.setAngle(rX2.getAngle() - 5 - r.nextInt(10));
+                rY2.setAngle(rY2.getAngle() - 5 - r.nextInt(10));
+                rZ2.setAngle(rZ2.getAngle() - 5 - r.nextInt(10));
+
+                if (isFaceUp(rX2)) {
                     System.out.println("Face aligning " + counter + " more times");
                     counter -= 1;
                 }
@@ -63,15 +88,19 @@ public class App extends Application {
         };
         Button roll = new Button("Roll");
         EventHandler<ActionEvent> event = _ -> {
-            counter = r.nextInt(6) + 6;
-            rX.setAngle(rX.getAngle() + 50);
-            rY.setAngle(rY.getAngle() + 50);
-            rZ.setAngle(rZ.getAngle() + 50);
-            timer.start();
+            counter = r.nextInt(6) + 3;
+            rX1.setAngle(rX1.getAngle() + 50);
+            rY1.setAngle(rY1.getAngle() + 50);
+            rZ1.setAngle(rZ1.getAngle() + 50);
+            rX2.setAngle(rX2.getAngle() + 50);
+            rY2.setAngle(rY2.getAngle() + 50);
+            rZ2.setAngle(rZ2.getAngle() + 50);
+            timer1.start();
+            timer2.start();
         };
         roll.setOnAction(event);
 
-        Group group = new Group(cameraTransform, dice, roll);
+        Group group = new Group(cameraTransform, dice1, dice2, roll);
 
         Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
         //scene.setFill(Color.BISQUE);
