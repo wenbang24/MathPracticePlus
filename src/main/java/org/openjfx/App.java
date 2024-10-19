b53a698cf12467417600efa575482e6657d42a60
wenbang24 <wenbangalt@gmail.com>
2024-10-19 19:56:46 +1100

    refactor: expression lambda instead of statement lambda

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -10,21 +10,15 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button diceRoller = new Button("Dice Roller");
-        diceRoller.setOnAction(_ -> {
-            new DiceRoller().DiceRollerStage(new Stage());
-        });
+        diceRoller.setOnAction(_ -> new DiceRoller().DiceRollerStage(new Stage()));
 
         Button quadraticSolver = new Button("Quadratic Solver");
         quadraticSolver.setTranslateY(35);
-        quadraticSolver.setOnAction(_ -> {
-            new QuadraticSolver().QuadraticSolverStage(new Stage());
-        });
+        quadraticSolver.setOnAction(_ -> new QuadraticSolver().QuadraticSolverStage(new Stage()));
 
         Button circleCalc = new Button("Circle Calculator");
         circleCalc.setTranslateY(70);
-        circleCalc.setOnAction(_ -> {
-            new CircleCalc().CircleCalcStage(new Stage());
-        });
+        circleCalc.setOnAction(_ -> new CircleCalc().CircleCalcStage(new Stage()));
 
         Group group = new Group(diceRoller, quadraticSolver, circleCalc);
         Scene scene = new Scene(group, 200, 150);
