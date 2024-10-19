9303458b5fba4267a390d7a5544b0afeccf5934f
wenbang24 <wenbangalt@gmail.com>
2024-10-19 18:47:59 +1100

    removed unused import, added lambda

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -20,12 +21,10 @@ import javafx.scene.transform.Translate;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.chart.LineChart;
 import javafx.stage.Stage;
 import org.fxyz3d.shapes.primitives.CuboidMesh;
 import org.fxyz3d.utils.CameraTransformer;
 import java.util.Random;
-import java.util.function.Function;
 
 public class App extends Application {
     int counter1 = 0;
@@ -40,97 +39,94 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button diceRoller = new Button("Dice Roller");
-        diceRoller.setOnAction(new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent unused) { // can't do underscore
-                PerspectiveCamera camera = new PerspectiveCamera(true);
-                camera.setNearClip(0.1);
-                camera.setFarClip(10000.0);
-                camera.setTranslateX(10);
-                camera.setTranslateZ(-100);
-                camera.setFieldOfView(18);
+        diceRoller.setOnAction(_ -> {
+            PerspectiveCamera camera = new PerspectiveCamera(true);
+            camera.setNearClip(0.1);
+            camera.setFarClip(10000.0);
+            camera.setTranslateX(10);
+            camera.setTranslateZ(-100);
+            camera.setFieldOfView(18);
 
-                CameraTransformer cameraTransform = new CameraTransformer();
-                cameraTransform.getChildren().add(camera);
+            CameraTransformer cameraTransform = new CameraTransformer();
+            cameraTransform.getChildren().add(camera);
 
-                CuboidMesh dice1 = new CuboidMesh(10f, 10f, 10f);
-                dice1.setTextureModeImage("texture.jpg");
-                CuboidMesh dice2 = new CuboidMesh(10f, 10f, 10f);
-                dice2.setTextureModeImage("texture.jpg");
-                Translate translate = new Translate(20, 0, 0);
-                dice2.getTransforms().add(translate);
+            CuboidMesh dice1 = new CuboidMesh(10f, 10f, 10f);
+            dice1.setTextureModeImage("texture.jpg");
+            CuboidMesh dice2 = new CuboidMesh(10f, 10f, 10f);
+            dice2.setTextureModeImage("texture.jpg");
+            Translate translate = new Translate(20, 0, 0);
+            dice2.getTransforms().add(translate);
 
-                Rotate rX1 = new Rotate(0, Rotate.X_AXIS);
-                Rotate rY1 = new Rotate(0, Rotate.Y_AXIS);
-                Rotate rZ1 = new Rotate(0, Rotate.Z_AXIS);
-                dice1.getTransforms().addAll(rX1, rY1, rZ1);
-                Rotate rX2 = new Rotate(0, Rotate.X_AXIS);
-                Rotate rY2 = new Rotate(0, Rotate.Y_AXIS);
-                Rotate rZ2 = new Rotate(0, Rotate.Z_AXIS);
-                dice2.getTransforms().addAll(rX2, rY2, rZ2);
-                Random r = new Random();
+            Rotate rX1 = new Rotate(0, Rotate.X_AXIS);
+            Rotate rY1 = new Rotate(0, Rotate.Y_AXIS);
+            Rotate rZ1 = new Rotate(0, Rotate.Z_AXIS);
+            dice1.getTransforms().addAll(rX1, rY1, rZ1);
+            Rotate rX2 = new Rotate(0, Rotate.X_AXIS);
+            Rotate rY2 = new Rotate(0, Rotate.Y_AXIS);
+            Rotate rZ2 = new Rotate(0, Rotate.Z_AXIS);
+            dice2.getTransforms().addAll(rX2, rY2, rZ2);
+            Random r = new Random();
 
-                AnimationTimer timer1 = new AnimationTimer() {
-                    @Override
-                    public void handle(long now) {
-                        rX1.setAngle(rX1.getAngle() + 5 + r.nextInt(10));
-                        rY1.setAngle(rY1.getAngle() + 5 + r.nextInt(10));
-                        rZ1.setAngle(rZ1.getAngle() + 5 + r.nextInt(10));
+            AnimationTimer timer1 = new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    rX1.setAngle(rX1.getAngle() + 5 + r.nextInt(10));
+                    rY1.setAngle(rY1.getAngle() + 5 + r.nextInt(10));
+                    rZ1.setAngle(rZ1.getAngle() + 5 + r.nextInt(10));
 
-                        if (isFaceUp(rX1)) {
-                            //System.out.println("Face 1 aligning " + counter1 + " more times");
-                            counter1 -= 1;
-                        }
-                        if (counter1 == 0) {
-                            stop();
-                        }
+                    if (isFaceUp(rX1)) {
+                        //System.out.println("Face 1 aligning " + counter1 + " more times");
+                        counter1 -= 1;
                     }
-                };
-                AnimationTimer timer2 = new AnimationTimer() {
-                    @Override
-                    public void handle(long now) {
-                        rX2.setAngle(rX2.getAngle() - 5 - r.nextInt(10));
-                        rY2.setAngle(rY2.getAngle() - 5 - r.nextInt(10));
-                        rZ2.setAngle(rZ2.getAngle() - 5 - r.nextInt(10));
-
-                        if (isFaceUp(rX2)) {
-                            //System.out.println("Face 2 aligning " + counter2 + " more times");
-                            counter2 -= 1;
-                        }
-                        if (counter2 == 0) {
-                            stop();
-                        }
+                    if (counter1 == 0) {
+                        stop();
                     }
-                };
-                Button roll = new Button("Roll");
-                Scale scale = new Scale(0.15, 0.15, 0.15);
-                Translate buttonTranslate = new Translate(47, -10, 0);
-                roll.getTransforms().addAll(scale, buttonTranslate);
-                EventHandler<ActionEvent> event = _ -> {
-                    counter1 = r.nextInt(6) + 3;
-                    counter2 = r.nextInt(6) + 3;
-                    rX1.setAngle(rX1.getAngle() + 50);
-                    rY1.setAngle(rY1.getAngle() + 50);
-                    rZ1.setAngle(rZ1.getAngle() + 50);
-                    rX2.setAngle(rX2.getAngle() + 50);
-                    rY2.setAngle(rY2.getAngle() + 50);
-                    rZ2.setAngle(rZ2.getAngle() + 50);
-                    timer1.start();
-                    timer2.start();
-                };
-                roll.setOnAction(event);
+                }
+            };
+            AnimationTimer timer2 = new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    rX2.setAngle(rX2.getAngle() - 5 - r.nextInt(10));
+                    rY2.setAngle(rY2.getAngle() - 5 - r.nextInt(10));
+                    rZ2.setAngle(rZ2.getAngle() - 5 - r.nextInt(10));
 
-                Group group = new Group(cameraTransform, dice1, dice2, roll);
+                    if (isFaceUp(rX2)) {
+                        //System.out.println("Face 2 aligning " + counter2 + " more times");
+                        counter2 -= 1;
+                    }
+                    if (counter2 == 0) {
+                        stop();
+                    }
+                }
+            };
+            Button roll = new Button("Roll");
+            Scale scale = new Scale(0.15, 0.15, 0.15);
+            Translate buttonTranslate = new Translate(47, -10, 0);
+            roll.getTransforms().addAll(scale, buttonTranslate);
+            EventHandler<ActionEvent> event = _ -> {
+                counter1 = r.nextInt(6) + 3;
+                counter2 = r.nextInt(6) + 3;
+                rX1.setAngle(rX1.getAngle() + 50);
+                rY1.setAngle(rY1.getAngle() + 50);
+                rZ1.setAngle(rZ1.getAngle() + 50);
+                rX2.setAngle(rX2.getAngle() + 50);
+                rY2.setAngle(rY2.getAngle() + 50);
+                rZ2.setAngle(rZ2.getAngle() + 50);
+                timer1.start();
+                timer2.start();
+            };
+            roll.setOnAction(event);
 
-                Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
-                //scene.setFill(Color.BISQUE);
-                scene.setCamera(camera);
+            Group group = new Group(cameraTransform, dice1, dice2, roll);
 
-                Stage diceStage = new Stage();
-                diceStage.setTitle("Dice Roller");
-                diceStage.setScene(scene);
-                diceStage.show();
-            }
+            Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
+            //scene.setFill(Color.BISQUE);
+            scene.setCamera(camera);
+
+            Stage diceStage = new Stage();
+            diceStage.setTitle("Dice Roller");
+            diceStage.setScene(scene);
+            diceStage.show();
         });
 
         Button quadraticSolver = new Button("Quadratic Solver");
