5b41c8139fccd0c10575545b3e7e18b7f72dca8e
wenbang24 <wenbangalt@gmail.com>
2024-10-21 14:08:50 +1100

    made isFaceUp static

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -10,18 +10,24 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button diceRoller = new Button("Dice Roller");
+        diceRoller.setMinWidth(200);
+        diceRoller.setMinHeight(100);
         diceRoller.setOnAction(_ -> new DiceRoller().start(new Stage()));
 
         Button quadraticSolver = new Button("Quadratic Solver");
-        quadraticSolver.setTranslateY(35);
+        quadraticSolver.setMinWidth(200);
+        quadraticSolver.setMinHeight(100);
+        quadraticSolver.setTranslateY(100);
         quadraticSolver.setOnAction(_ -> new QuadraticSolver().start(new Stage()));
 
         Button circleCalc = new Button("Circle Calculator");
-        circleCalc.setTranslateY(70);
+        circleCalc.setMinWidth(200);
+        circleCalc.setMinHeight(100);
+        circleCalc.setTranslateY(200);
         circleCalc.setOnAction(_ -> new CircleCalc().start(new Stage()));
 
         Group group = new Group(diceRoller, quadraticSolver, circleCalc);
-        Scene scene = new Scene(group, 200, 150);
+        Scene scene = new Scene(group, 200, 300);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Math Practice Plus");
--- src/main/java/org/openjfx/DiceRoller.java
+++ src/main/java/org/openjfx/DiceRoller.java
@@ -21,7 +21,7 @@ public class DiceRoller extends Application {
     int counter1 = 0;
     int counter2 = 0;
 
-    private boolean isFaceUp(Rotate rotateX) {
+    private static boolean isFaceUp(Rotate rotateX) {
         double threshold = 2;  // allow a margin of error
         double angleX = Math.abs(rotateX.getAngle() % 360);
         return (Math.abs(angleX) < threshold) || (Math.abs(angleX - 90) < threshold) || (Math.abs(angleX - 180) < threshold) || (Math.abs(angleX - 270) < threshold);
@@ -64,7 +64,6 @@ public class DiceRoller extends Application {
                 rZ1.setAngle(rZ1.getAngle() + 5 + r.nextInt(10));
 
                 if (isFaceUp(rX1)) {
-                    //System.out.println("Face 1 aligning " + counter1 + " more times");
                     counter1 -= 1;
                 }
                 if (counter1 == 0) {
@@ -80,7 +79,6 @@ public class DiceRoller extends Application {
                 rZ2.setAngle(rZ2.getAngle() - 5 - r.nextInt(10));
 
                 if (isFaceUp(rX2)) {
-                    //System.out.println("Face 2 aligning " + counter2 + " more times");
                     counter2 -= 1;
                 }
                 if (counter2 == 0) {
