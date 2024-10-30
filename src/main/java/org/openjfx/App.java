9e20dcc6771387e8c1c7aada3c67a03336d51949
wenbang24 <wenbangalt@gmail.com>
2024-10-30 15:29:36 +1100

    comments

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -9,23 +9,30 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
+        // dice roller button
         Button diceRoller = new Button("Dice Roller");
         diceRoller.setMinWidth(200);
         diceRoller.setMinHeight(100);
+        // create new instance of diceRoller class
         diceRoller.setOnAction(action -> new DiceRoller().start(new Stage()));
 
+        // quadratic solver button
         Button quadraticSolver = new Button("Quadratic Solver");
         quadraticSolver.setMinWidth(200);
         quadraticSolver.setMinHeight(100);
         quadraticSolver.setTranslateY(100);
+        // create new instance of quadraticSolver class
         quadraticSolver.setOnAction(action -> new QuadraticSolver().start(new Stage()));
 
+        // circle calculator button
         Button circleCalc = new Button("Circle Calculator");
         circleCalc.setMinWidth(200);
         circleCalc.setMinHeight(100);
         circleCalc.setTranslateY(200);
+        // create new instance of circleCalc class
         circleCalc.setOnAction(action -> new CircleCalc().start(new Stage()));
 
+        // group buttons together and add to scene
         Group group = new Group(diceRoller, quadraticSolver, circleCalc);
         Scene scene = new Scene(group, 200, 300);
 
